6cf62d21587135cb76604e16d9cff4a2a9f1c59a
CrazyCyfro <crazycyfro@gmail.com>
2015-04-27 22:36:53 +0800

    i think i did some stuff

--- app/src/main/java/com/mtndew/doritos/homeworkapp/HomeworkDetailFragment.java
+++ app/src/main/java/com/mtndew/doritos/homeworkapp/HomeworkDetailFragment.java
@@ -5,13 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-
-import java.util.ArrayList;
-
-import com.mtndew.doritos.homeworkapp.HomeworkContent;
 
 /**
  * A fragment representing a single Homework detail screen.
